90ca7311522d999400206f0a717e18fd1fec7138
Wolf-Hong <925372932@qq.com>
2015-01-28 17:16:59 +0800

    Initial Commit

---
+++ gitlianxi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitlianxi
+//
+//  Created by qihonggang on 15-1-28.
+//  Copyright (c) 2015年 齐宏刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitlianxi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitlianxi
+//
+//  Created by qihonggang on 15-1-28.
+//  Copyright (c) 2015年 齐宏刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
